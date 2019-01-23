bfb435eba27a7a46d3542bff142a7e47c25cd906
Rami <rami.djema@mail.mcgill.ca>
2019-01-23 14:20:25 -0500

    Added env variable for database url

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const http = require('http');
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.2';
 const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -9,3 +10,17 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+var MongoClient = require('mongodb').MongoClient;
+var url = process.env.DATABASE_URL;
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("carowanisdb");
+    var query = { _id: "1" };
+    dbo.collection("readings").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();   
+    });
+  });
