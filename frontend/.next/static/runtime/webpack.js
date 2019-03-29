4e20dae1e93b10c9616f5c924dba4e13a8a35200
AlexisGJ <alexis.giguere-joannette@mail.mcgill.ca>
2019-03-29 13:36:06 -0400

    Updated start script

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e6ba0b09f2469655c30a";
+/******/ 	var hotCurrentHash = "cf384a4213e12db7fa84";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
